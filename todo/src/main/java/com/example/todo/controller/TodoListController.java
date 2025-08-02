d9e4bd66a9a34ac78c2f98a3c7c5b7b42286ba26
krishnamadar <krishnamadar110@gmail.com>
2025-08-03 00:46:56 +0530

    Krishna | Adding title and get all task

--- todo/src/main/java/com/example/todo/controller/TodoListController.java
+++ todo/src/main/java/com/example/todo/controller/TodoListController.java
@@ -1,7 +1,26 @@
 package com.example.todo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.todo.entity.TodoList;
+import com.example.todo.service.TodoListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("/api/todos")
 @RestController
 public class TodoListController {
+    @Autowired
+    TodoListService service;
+
+    @PostMapping("/add")
+    public void addTodo(@RequestBody TodoList todoList) {
+        service.addTodo(todoList);
+        System.out.println("Title added successfully!!");
+    }
+
+    @GetMapping("/getall")
+    public List<TodoList> getAll() {
+        return service.getAll();
+    }
 }
--- todo/src/main/java/com/example/todo/service/TodoListService.java
+++ todo/src/main/java/com/example/todo/service/TodoListService.java
@@ -1,4 +1,23 @@
 package com.example.todo.service;
 
-public interface TodoListService {
+import com.example.todo.entity.TodoList;
+import com.example.todo.repository.TodoListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoListService {
+    @Autowired
+    TodoListRepository repo;
+
+    public void addTodo(TodoList todoList) {
+        repo.save(todoList);
+    }
+
+    public List<TodoList> getAll() {
+        return repo.findAll();
+    }
 }
+
