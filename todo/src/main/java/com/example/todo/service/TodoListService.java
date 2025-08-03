9f9c77ffaada86439d1865c543378a331ac13125
nikhil <nikhilhallikeri7@gmail.com>
2025-08-04 01:25:57 +0530

    Nikhil | Added Priority field for TodoList Entity and CRUD operation along with findByName

--- todo/src/main/java/com/example/todo/service/TodoListService.java
+++ todo/src/main/java/com/example/todo/service/TodoListService.java
@@ -6,18 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoListService {
     @Autowired
     TodoListRepository repo;
 
-    public void addTodo(TodoList todoList) {
-        repo.save(todoList);
+    public List<TodoList> getAllTodos() {
+        return repo.findAll();
     }
 
-    public List<TodoList> getAll() {
-        return repo.findAll();
+    public Optional<TodoList> getTodoById(Long id) {
+        return repo.findById(id);
+    }
+
+    public Optional<TodoList> findByTitle(String title){
+        return repo.findByTitle(title);
+    }
+
+    public TodoList addTodo(TodoList todoList) {
+        TodoList saved = repo.save(todoList);
+        return repo.findById(saved.getId()).orElse(saved);  // GOOD: this reloads from DB
+    }
+
+    public TodoList updateTodo(Long id, TodoList updatedTodo) {
+        TodoList todo = repo.findById(id).orElseThrow(() -> new RuntimeException("Todo not found!"));
+
+        todo.setTitle(updatedTodo.getTitle());
+        todo.setStatus(updatedTodo.isStatus());
+        todo.setPriority(updatedTodo.getPriority());
+
+        return repo.save(todo);
+    }
+
+    public void deleteTodo(Long id) {
+        repo.deleteById(id);
     }
 }
 
